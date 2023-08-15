72be9a6bf645ac0d7a86d59f5b2549d0b288637c
novemberizing <novemberizing@gmail.com>
2023-08-15 15:20:33 +0900

    Remove console.log

--- src/IdentityAccessManager.js
+++ src/IdentityAccessManager.js
@@ -20,6 +20,33 @@ export default class IdentityAccessManager extends ApplicationServerService {
         Application.use(IdentityAccessUser);
     }
 
+    /** 어디에 위치하는 것이 좋을까? */
+    static #authorization(o, type, ...args) {
+        if(type === 'http') {
+            const req = args[0];
+
+            const authorization = _.split(req.header("Authorization"), " ");
+            if(authorization[0] === 'Google') {
+                o.google = authorization[1];
+            } else if(authorization[0] === 'Bearer') {
+                o.access = authorization[1];
+            }
+
+            return o;
+        }
+
+        /** 현재는 지원하지 않는다. HTTP 말고 SOCKET 등도 구현할 예정이다. */
+        throw new IdentityAccessExceptionUnsupported();
+    }
+
+    static #error(e) {
+        console.log(e);
+        return {
+            name: e.name,
+            message: e.message
+        }
+    }
+
     constructor(server, config) {
         super("/iam", server, config);
 
@@ -30,44 +57,60 @@ export default class IdentityAccessManager extends ApplicationServerService {
         if(!this.modules.get("/token")) this.reg(new IdentityAccessTokenizer(this, {}));
 
         if(server.express) {
-            server.express.get(`${this.path}/signin`, async (req, res) => {
-                try {
-                    // check auth token
-                    const o = {
-                        account: {
-                            identity: req.query.identity,
-                            password: req.query.password
-                        }
-                    };
-
-                    const authorization = _.split(req.header("Authorization"), " ");
-                    console.log(authorization);
-                    if(authorization[0] === 'Google') {
-                        o.google = authorization[1];
-                    } else if(authorization[0] === 'Bearer') {
-                        o.access = authorization[1];
-                    }
-
-                    res.send(await this.signin(o));
-                } catch(e) {
-                    console.log(e);
-                    res.status(500).send({});
-                }
+            server.express.get(`${this.path}/user/check`, async (req, res) => {
+                /** 파라미터가 없는 것이 마음에 들지 않는다. */
+                await IdentityAccessManager.call(async () => res.send(await this.check(IdentityAccessManager.#authorization({}, 'http', req))),
+                                                       e  => res.status(500).send(IdentityAccessManager.#error(e)));
             });
+
+            server.express.get(`${this.path}/signin`, async (req, res) => {
+                const o = {
+                    account: {
+                        identity: req.query.identity,
+                        password: req.query.password
+                    }
+                };
+                await IdentityAccessManager.call(async () => res.send(await this.signin(IdentityAccessManager.#authorization(o, 'http', req))),
+                                                       e  => res.status(500).send(IdentityAccessManager.#error(e)));
+            });
+
             server.express.post(`${this.path}/signup`, async (req, res) => {
-                try {
-                    res.send(await this.signup(req.body.account, req.body.user));
-                } catch(e) {
-                    res.status(500).send({});
-                }
+                await IdentityAccessManager.call(async () => res.send(await this.signup(req.body.account, req.body.user)),
+                                                       e  => res.status(500).send(IdentityAccessManager.#error(e)));
             });
         }
     }
 
+    async check(o) {
+        if(o.access) {
+            return await this.moduleCall("/token", "access", o.access);
+        }
+
+        if(o.google) {
+            const ticket = await this.moduleCall("/authenticator", "authenticate", { google: { credential: o.google }});
+            let user = await this.moduleCall("/user", "get", { email: ticket.payload.email });
+
+            if(user === undefined) {
+                // 사용자가 존재하지 않으면 사용자를 생성한다.
+                user = await this.moduleCall("/user", "gen", {
+                    email: ticket.payload.email,
+                    name: `${ticket.payload.given_name} ${ticket.payload.family_name}`,
+                    profile: ticket.payload.picture
+                });
+            }
+
+            const token = await this.moduleCall("/token", "gen", { user }, { google: { credential: o.google }});
+
+            return { user, token };
+        }
+
+        throw new IdentityAccessExceptionUnsupported();
+    }
+
     // account, token, google
     async signin(o) {
-        if(o.token && o.token.access) {
-            return await this.moduleCall("/token", "access", o.token.access);
+        if(o.access) {
+            return await this.moduleCall("/token", "access", o.access);
         }
 
         if(o.google) {
@@ -112,7 +155,8 @@ export default class IdentityAccessManager extends ApplicationServerService {
     async signup(account, user) {
         user = await this.moduleCall("/user", "gen", user);
         account = await this.moduleCall("/account", "gen", Object.assign({ no: user.no }, account));
+        const token = await this.moduleCall("/token", "gen", { user }, { account });
 
-        return { account, user };
+        return { account, user, token };
     }
 }
